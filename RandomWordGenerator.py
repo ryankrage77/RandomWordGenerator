c0cbe6c94ed83b0dd5a687ed2575808d18518cf9
ryankrage77 <ryankrage77@gmail.com>
2018-02-12 20:33:35 +0000

    version 1
    crashes on length 0, words are only barely pronounceable.

--- RandomWordGenerator.py
+++ RandomWordGenerator.py
@@ -2,14 +2,44 @@
 #Generates random pronouncable strings of a specified length.
 
 import random
+word = []
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+vowels = "aeiou"
+currentLetter = ""
+genLength = 0
 
 while True:
     stringLength = input("String length: ")
-    while Genlength <= stringLength:
-        #pick random letter
-        #check  if letter is consonant or vowel
-        #if vowel, go back through word by up to 3 places to check for vowels
-        #reject letter if they are, add to list if not
-        #if consonant, check if up to last 2 letters are consonants
-        #reject if they are, add if not
-        #repeat?
+    while genLength <= (stringLength - 1):
+        currentLetter = random.choice(alphabet)
+        if currentLetter in vowels:
+            if genLength >= 3:
+                if word[(genLength - 1)] in vowels and word[(genLength - 2)] in vowels and word[(genLength - 3)] in vowels:
+                    print "Letter rejected, more than 3 vowels in a row"
+                else:
+                    word.append(currentLetter)
+                    genLength += 1
+                    print word
+            else:
+                word.append(currentLetter)
+                genLength += 1
+                print word
+        else:
+            if genLength >= 2:
+                if word[genLength - 1] not in vowels and word[genLength - 2] not in vowels:
+                    print "Letter rejected, too many consonants in a row"
+                else:
+                    word.append(currentLetter)
+                    genLength += 1
+                    print word
+            else:
+                word.append(currentLetter)
+                genLength += 1
+                print word
+
+    word[0] = word[0].upper()
+    fullWord = "".join(str(x) for x in word)
+    print "Generated Word: ", fullWord
+    genLength = 0
+    word = []
+    continue
