bb158b40abbf681f16b24c360de85287516cb0e5
Ryan Krage <ryankrage77@gmail.com>
2018-02-13 12:37:51 +0000

    added Python3 support

--- RandomWordGenerator.py
+++ RandomWordGenerator.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 #Ryankrage77's Random Word Generator
 #Generates random pronouncable strings of a specified length.
 
@@ -11,36 +11,36 @@ genLength = 0
 
 while True:
     stringLength = input("String length: ")
-    while genLength <= (stringLength - 1):
+    while genLength <= (int(stringLength) - 1):
         currentLetter = random.choice(alphabet)
         if currentLetter in vowels:
             if genLength >= 3:
                 if word[(genLength - 1)] in vowels and word[(genLength - 2)] in vowels and word[(genLength - 3)] in vowels:
-                    print "Letter rejected, more than 3 vowels in a row"
+                    print("Letter rejected, more than 3 vowels in a row")
                 else:
                     word.append(currentLetter)
                     genLength += 1
-                    print word
+                    print(word)
             else:
                 word.append(currentLetter)
                 genLength += 1
-                print word
+                print(word)
         else:
             if genLength >= 2:
                 if word[genLength - 1] not in vowels and word[genLength - 2] not in vowels:
-                    print "Letter rejected, too many consonants in a row"
+                    print("Letter rejected, more than 2 consonants in a row")
                 else:
                     word.append(currentLetter)
                     genLength += 1
-                    print word
+                    print(word)
             else:
                 word.append(currentLetter)
                 genLength += 1
-                print word
+                print(word)
 
     word[0] = word[0].upper()
     fullWord = "".join(str(x) for x in word)
-    print "Generated Word: ", fullWord
+    print("Generated Word: ", fullWord)
     genLength = 0
     word = []
     continue
