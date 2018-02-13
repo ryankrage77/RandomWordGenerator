6539c8881ebe910196ddeba5ef154f56c75edc86
Ryan Krage <ryankrage77@gmail.com>
2018-02-13 16:20:19 +0000

    removed extra prints
    removed prints from main loop so it just spits the word at you instead.
    
    Looks faster, cleaner, and doesn't clog up terminal.

--- RandomWordGenerator.py
+++ RandomWordGenerator.py
@@ -16,27 +16,29 @@ while True:
         if currentLetter in vowels:
             if genLength >= 3:
                 if word[(genLength - 1)] in vowels and word[(genLength - 2)] in vowels and word[(genLength - 3)] in vowels:
-                    print("Letter rejected, more than 3 vowels in a row")
+                    #print("Letter rejected, more than 3 vowels in a row")
+                    pass
                 else:
                     word.append(currentLetter)
                     genLength += 1
-                    print(word)
+                    #print(word)
             else:
                 word.append(currentLetter)
                 genLength += 1
-                print(word)
+                #print(word)
         else:
             if genLength >= 2:
                 if word[genLength - 1] not in vowels and word[genLength - 2] not in vowels:
-                    print("Letter rejected, more than 2 consonants in a row")
+                    #print("Letter rejected, more than 2 consonants in a row")
+                    pass
                 else:
                     word.append(currentLetter)
                     genLength += 1
-                    print(word)
+                    #print(word)
             else:
                 word.append(currentLetter)
                 genLength += 1
-                print(word)
+                #print(word)
 
     word[0] = word[0].upper()
     fullWord = "".join(str(x) for x in word)
